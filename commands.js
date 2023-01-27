698a0effbfb3f3971774da52f6dacfb0d299a40d
cacero <caacero@indracompany.com>
2023-01-27 13:11:31 -0500

    Start with dotnet .net

---
+++ commands.js
@@ -0,0 +1,14 @@
+const commands = [
+    {
+        name : "dotnet new --list",
+        desc : "show the list of possible templates for create a dotnet project"
+    },
+    {
+        name : "dotnet new webapi",
+        desc : "create the template for a API"
+    },
+    {
+        name : "dotnet watch run",
+        desc : "run the current project"
+    }
+]
